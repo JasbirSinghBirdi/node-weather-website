cbbd3c07eae0593f73e35d80770480147e3b0ccb
Jasbir Singh Birdi <jsinghbi@isimtl.local>
2020-07-19 02:58:23 -0400

    Added more Forecast content

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degree out, It feels like " + body.current.feelslike + " degree out")
+            callback(undefined, body.current.weather_descriptions[0]+ ", Humidity " + body.current.humidity + "% " + ". It is currently " + body.current.temperature + " degree out, It feels like " + body.current.feelslike + " degree out.")
+
         }
     })
 }
